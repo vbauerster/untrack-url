d9521ecb29daa4c02ba37b960794d7052db11e62
Vladimir Bauer <vbauerster@gmail.com>
2016-11-24 23:17:30 +0500

    TestRemoveAdsArbitraryParam

--- main_test.go
+++ main_test.go
@@ -26,7 +26,7 @@ func TestFollow(t *testing.T) {
 	}
 }
 
-func TestRemoveKnownAds(t *testing.T) {
+func TestRemoveAds(t *testing.T) {
 	ali := "https://ru.aliexpress.com/store/product/Original-Xiaomi-Mi5s-Mi-5S-3GB-RAM-64GB-ROM-Mobile-Phone-Snapdragon-821-5-15-1920x1080/311331_32740701280.html?aff_platform=aaf&sk=VnYZvQVf%3A&cpt=1479995730630&dp=bc1f2bd78b3dbf51260453f6b915ce98&af=288795&cv=47843&afref=&aff_trace_key=e8364f22d3e546fcafe1cf5b61b9519a-1479995730630-04043-VnYZvQVf"
 	gear := "http://www.gearbest.com/cell-phones/pp_471491.html?wid=21&admitad_uid=75984b1e4bfdbbda9d4238493f856147"
 	cool := "http://www.coolicool.com/xiaomi-mi-5s-ultrasonic-fingerprint-3gb-ram-32gb-rom-qualcomm-snapdragon-821-215ghz-quad-core-515-g-44250?utm_source=admitad&utm_medium=referral&admitad_uid=953908711f4f569b3e8acdf0f3ef7ba6"
@@ -60,25 +60,44 @@ func TestRemoveKnownAds(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		// url, err := url.Parse(test.in)
-		// if err != nil {
-		// 	t.Fatal(err)
-		// }
-		// got := removeAds(test.in)
-		// if len(test.dir.ParamsToDel) != 0 {
-		// 	url, err := url.Parse(got)
-		// 	if err != nil {
-		// 		t.Fatal(err)
-		// 	}
-		// 	v := url.Query()
-		// 	for _, p := range test.dir.ParamsToDel {
-		// 		if _, ok := v[p]; ok {
-		// 			t.Errorf("param %q wasn't removed\n", p)
-		// 		}
-		// 	}
-		// }
 		if got := removeAds(test.in); got != test.out {
 			t.Errorf("\nExpected: %q\nGot: %q\n", test.out, got)
+		}
+	}
+}
+
+func TestRemoveAdsArbitraryParam(t *testing.T) {
+	var tests = []struct {
+		in  string
+		dir directive
+	}{
+		{
+			"https://example.org/test?spy_ad=toberemoved",
+			directive{ParamsToDel: []string{"spy_ad"}},
+		},
+		{
+			"https://example.org/test?spy_ad=toberemoved&wid=foo",
+			directive{ParamsToDel: []string{"wid"}},
+		},
+	}
+	for _, test := range tests {
+		url, err := url.Parse(test.in)
+		if err != nil {
+			t.Fatal(err)
+		}
+		locations[url.Host] = test.dir
+		got := removeAds(test.in)
+		if len(test.dir.ParamsToDel) != 0 {
+			url, err := url.Parse(got)
+			if err != nil {
+				t.Fatal(err)
+			}
+			v := url.Query()
+			for _, p := range test.dir.ParamsToDel {
+				if _, ok := v[p]; ok {
+					t.Errorf("param %q wasn't removed\n", p)
+				}
+			}
 		}
 	}
 }
