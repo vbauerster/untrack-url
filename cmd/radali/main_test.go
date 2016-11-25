38ad514a608f77f18e0190582a9e94049019f522
Vladimir Bauer <vbauerster@gmail.com>
2016-11-25 19:17:12 +0500

    TestParseURL

--- cmd/radali/main_test.go
+++ cmd/radali/main_test.go
@@ -114,3 +114,24 @@ func TestRemoveAdsArbitraryParam(t *testing.T) {
 		}
 	}
 }
+
+func TestParseURL(t *testing.T) {
+	tests := []struct {
+		in   string
+		want string
+	}{
+		{"https://golang.org", "https://golang.org"},
+		{"https://golang.org:443/test", "https://golang.org:443/test"},
+		{"localhost:8080/test", "https://localhost:8080/test"},
+		{"localhost:80/test", "http://localhost:80/test"},
+		{"//localhost:8080/test", "https://localhost:8080/test"},
+		{"//localhost:80/test", "http://localhost:80/test"},
+	}
+
+	for _, test := range tests {
+		u := parseURL(test.in)
+		if u.String() != test.want {
+			t.Errorf("Given: %s\nwant: %s\ngot: %s", test.in, test.want, u.String())
+		}
+	}
+}
